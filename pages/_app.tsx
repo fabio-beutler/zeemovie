6257a9ec08b4434c8b893f5f9eab1407d12c95d8
Fabio Beutler <dev.fabiobeutler@gmail.com>
2022-07-09 16:45:02 -0400

    style: :heavy_plus_sign: Add initial global styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
-import '../styles/globals.css'
+import { globalStyles } from '@/styles/global'
 import type { AppProps } from 'next/app'
 
+import '@fontsource/roboto/400.css'
+import '@fontsource/roboto/500.css'
+import '@fontsource/roboto/700.css'
+import '@fontsource/roboto/900.css'
+
 function MyApp({ Component, pageProps }: AppProps) {
+  globalStyles()
   return <Component {...pageProps} />
 }
 
---
+++ pages/_document.tsx
@@ -0,0 +1,19 @@
+import { Html, Head, Main, NextScript } from 'next/document'
+import { getCssText } from '@/styles/stitches.config'
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <style
+          id='stitches'
+          dangerouslySetInnerHTML={{ __html: getCssText() }}
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  )
+}
